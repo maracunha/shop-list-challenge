e35d3614fa4ceef1d55aacfce24a8443a0284b74
Renan Marangon <maracunha@gmail.com>
2023-02-09 14:55:20 -0300

    fix: To go to products page

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -31,7 +31,7 @@ const Login = () => {
 
   useEffect(() => {
     if (userState.token) {
-      navigate('/', { replace: true });
+      navigate('/products', { replace: true });
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [userState.token]);
